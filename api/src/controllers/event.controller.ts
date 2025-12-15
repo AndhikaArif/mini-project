793a2536285f36a88fe72c6c8b77ee87e62b3475
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2025-12-15 10:00:12 +0700

    add new feature

--- api/src/controllers/event.controller.ts
+++ api/src/controllers/event.controller.ts
@@ -70,6 +70,30 @@ export class EventController {
     }
   }
 
+  async getTopThreeEvents(req: Request, res: Response) {
+    try {
+      const events = await eventService.getTopThreeEvents();
+
+      res.status(200).json(events);
+    } catch (error) {
+      res.status(500).json({ message: "Failed to get top three events" });
+    }
+  }
+
+  async getEventsByCategory(req: Request, res: Response) {
+    try {
+      let page = Number(req.query.page);
+      if (!page || page < 1) page = 1;
+
+      const { events, totalData, totalPages } =
+        await eventService.getEventsByCategory(page);
+
+      res.status(200).json({ events, totalData, totalPages });
+    } catch (error) {
+      res.status(500).json({ message: "Failed to get events" });
+    }
+  }
+
   async updateEvent(req: Request, res: Response) {
     try {
       const id = String(req.params.id);
