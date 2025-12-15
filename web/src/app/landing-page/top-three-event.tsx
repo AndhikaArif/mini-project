22b1953c6d9de7ee9767e08c71ddb88ba62192af
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2025-12-15 14:11:52 +0700

    add new section: events

--- web/src/app/landing-page/top-three-event.tsx
+++ web/src/app/landing-page/top-three-event.tsx
@@ -1,6 +1,6 @@
 export default function () {
   return (
-    <section className="flex flex-col justify-center items-start px-5 md:px-50 bg-gray-100 w-full h-[300px] mt-10">
+    <section className="flex flex-col justify-center items-start px-6 md:px-50 bg-gray-100 w-full h-[300px] mt-20">
       <h2>Top 3 Events</h2>
       <h2>Event 1</h2>
       <h2>Event 2</h2>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 import BannerSection from "./landing-page/banner";
-import CategorySection from "./landing-page/category";
 import TopThreeEvent from "./landing-page/top-three-event";
-import topThreeEvent from "./landing-page/top-three-event";
+import CategorySection from "./landing-page/category";
+import EventsSection from "./landing-page/events";
 
 export default function DashboardPage() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-auto pb-30">
       <BannerSection />
       <TopThreeEvent />
       <CategorySection />
+      <EventsSection />
     </main>
   );
 }
